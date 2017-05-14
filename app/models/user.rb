4a717ffaab6c68df4ab056e0406309273c65e7cf
Kevin Hsieh <kshsieh@wework.com>
2017-05-14 07:10:28 +0300

    fixup user real quick

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@
 
 class User < ActiveRecord::Base
   validates_uniqueness_of :uuid, :email
+  validates_presence_of :name, :email, :uuid
 
-  before_save :ensure_uuid
+  before_validation :ensure_uuid
 
   private
 
