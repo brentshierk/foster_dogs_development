107d1d777bc94b176f234a75bea244b6080cf130
Kevin Hsieh <kshsieh@wework.com>
2017-05-14 07:25:53 +0300

    dog age

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     status += " with #{link_to(dog.current_status.user.name, user_path(dog.current_status.user))}" if dog.current_status.user.present?
     status.html_safe
   end
+
+  def age(dog)
+    years = (Date.current - dog.birthday).to_f/(30 * 12)
+    half = years.floor + 0.5
+    years > half ? "#{years.floor} and a half years old" : "#{years.floor} years old"
+  end
 end
