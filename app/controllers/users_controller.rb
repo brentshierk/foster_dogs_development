989c2f17e53fef61fc9d5316e67a139c2cb12583
Kevin Hsieh <kevinshsieh@gmail.com>
2017-06-27 16:23:08 -0400

    basic filtering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
   def index
-    @users = User.page(params[:page])
+    query = User.all
+
+    if taggable_filter_params.present?
+      taggable_filter_params.each_pair do |key, values|
+        query = query.tagged_with(values, in: key)
+      end
+    end
+
+    @users = query.page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def taggable_filter_params
+    params.permit(:size_preference, :experience, :size_preferences, :activity_preferences, :schedule)
   end
 end
