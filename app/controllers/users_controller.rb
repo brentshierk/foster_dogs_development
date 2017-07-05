0461047d9bea27734a4d43ae8cb1347140978068
Kevin Hsieh <kevinshsieh@gmail.com>
2017-07-05 19:24:51 -0400

    filters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  before_action :load_filter_categories, only: :index
+
   def index
     query = User.all
 
     if taggable_filter_params.present?
+      @active_filters = taggable_filter_params
+      
       taggable_filter_params.each_pair do |key, values|
         query = query.tagged_with(values, in: key)
       end
@@ -18,6 +22,20 @@ class UsersController < ApplicationController
   private
 
   def taggable_filter_params
-    params.permit(:size_preference, :experience, :size_preferences, :activity_preferences, :schedule)
+    params.permit(
+      :experience, :size_preferences, :activity_preferences, :schedule, # used if clicking through a url
+      experience: [], size_preferences: [], activity_preferences: [], schedule: [] # used through form
+    )
+  end
+
+  def load_filter_categories
+    @categories = Hash.new
+
+    ActsAsTaggableOn::Tagging
+      .includes(:tag)
+      .where(taggable_type: "User")
+      .group_by(&:context).each do |k, v|
+        @categories[k] = v.map { |r| r.tag.name }.compact.uniq
+      end
   end
 end
