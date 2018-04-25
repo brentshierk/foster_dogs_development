cb0cb9268524eed143ad4051e4997740484ab304
Kevin Hsieh <kevinshsieh@gmail.com>
2018-04-25 17:00:55 -0400

    mailchimp integration

--- app/services/mailchimp_service.rb
+++ app/services/mailchimp_service.rb
@@ -1,9 +1,20 @@
 class MailchimpService
   attr_accessor :api
 
-  MASTER_LIST_ID = '934017' # foster roster master list
+  MASTER_LIST_ID = 'f82fbab61a' # foster roster master list
 
   def initialize
     @api = Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
+  end
+
+  def list_members
+    api.lists.members(MASTER_LIST_ID)['data']
+  end
+
+  def subscribe_user(user)
+    api.lists.subscribe(
+      MASTER_LIST_ID,
+      { email: user.email }
+    )
   end
 end
