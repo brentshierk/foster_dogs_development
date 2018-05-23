75d041d114736a77808849d54b116eccdfbef3b1
Kevin Hsieh <kevinshsieh@gmail.com>
2018-05-23 08:10:03 -0700

    meh bring this back

---
+++ app/models/status.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: statuses
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  dog_id     :integer
+#  status     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Status < ApplicationRecord
+  NEEDS_FOSTER = 'Needs Foster'
+  FOSTER_PENDING = 'Foster Pending'
+  IN_FOSTER = 'In Foster'
+  ADOPTION_PENDING = 'Adoption Pending'
+  ADOPTED = 'Adopted'
+
+  STATUSES = [NEEDS_FOSTER, FOSTER_PENDING, IN_FOSTER, ADOPTION_PENDING, ADOPTED]
+
+  belongs_to :user
+  belongs_to :dog
+end
