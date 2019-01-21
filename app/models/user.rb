37d793e6528fda995860d8218396f51f4d7f45f3
Kevin Hsieh <kevinshsieh@gmail.com>
2019-01-21 17:28:10 -0500

    remove acts_as_taggable and cutover code

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,23 +41,9 @@ class User < ApplicationRecord
   has_many :outreaches_users
   has_many :outreaches, through: :outreaches_users
 
-  # TODO: delete me
-  acts_as_taggable
-  acts_as_taggable_on :experience, :schedule, :size_preferences, :activity_preferences
-
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? && obj.address_changed? }
   after_commit :subscribe_to_mailchimp, on: :create
-
-  # TODO: delete me
-  SIZE_PREFERENCES = {
-    "Small" => "Up to 25 lbs.",
-    "Medium" => "25-50 lbs.",
-    "Large" => "50 lbs and up"
-  }
-  EXPERIENCE = ["Dogsat", "Fostered", "Own / Owned a dog", "Volunteered", "Other"]
-  SCHEDULE = ["Almost never (0-3 hrs/day)", "4-7 hours per day", "8+ hours per day"]
-  ACTIVITY_PREFERENCES = ["Low activity", "Moderately active", "Active", "Young puppy"]
 
   scope :subscribed, -> { where.not(subscribed_at: nil).where(unsubscribed_at: nil) }
 
@@ -65,10 +51,6 @@ class User < ApplicationRecord
     subscribed
     .includes(outreaches: :organization)
     .order('created_at DESC')
-  end
-
-  def self.size_preferences
-    tag_counts_on("size_preferences").map(&:name).uniq.sort
   end
 
   def first_name
@@ -81,61 +63,6 @@ class User < ApplicationRecord
 
   def active?
     !subscribed_at.nil? && unsubscribed_at.nil?
-  end
-
-  # TODO: delete me
-  def has_preference?(preference_category, preference)
-    # acts_as_taggable has a weird thing with the tag name where only the first letter can be capitalized
-    # Own / Owned a dog is turned to Own / owned a dog which results in a mismatch
-    # downcase on both sides to sanitize before comparison
-    send("#{preference_category}_list").map(&:downcase).include?(preference.downcase)
-  end
-
-  # used to translate the question slug data schema and return the taggable schema
-  def question_to_preferences(question)
-    data =  case question.slug.to_sym
-            when :fostered_before, :fospice, :fosters_cats, :other_pets, :kids
-              send(question.slug)
-            when :fostered_for
-              fostered_for.map { |r| ["Heavenly Angels", "Foster Dogs"].include?(r) ? "Other" : r }
-            when :size
-              size_preference_list.map { |r| "#{r} (#{SIZE_PREFERENCES[r]})" }
-            when :experience
-              experience_list.map do |r|
-                if ["Own / owned a dog", "Own / owned"].include?(r)
-                  "Own / Owned a dog"
-                elsif r == "Never cared for an animal before"
-                  "Other"
-                else
-                  r
-                end
-              end
-            when :schedule
-              schedule_list.map { |r| r == "Incomplete" ? "Almost never (0-3 hrs/day)" : r }
-            when :activity
-              activity_preference_list
-            when :fosters_big_dogs
-              big_dogs
-            else
-              raise StandardError, "Slug #{question.slug} not registered!"
-            end
-
-    validate_preferences!(answer: data, choices: question.question_choices)
-
-    data
-  end
-
-  def validate_preferences!(answer:, choices:)
-    return true if answer.nil?
-    return true unless choices.present?
-
-    verdict = if answer.is_a?(Array)
-                answer.all? { |r| choices.include?(r.to_s) }
-              else
-                choices.include?(answer.to_s)
-              end
-    debugger if verdict == false
-    raise 'preference does not match provided list!' unless verdict
   end
 
   private
