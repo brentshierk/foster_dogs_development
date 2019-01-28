76f019c36d04a55b8664afff9f291bf43a2fa70a
Kevin Hsieh <kevinshsieh@gmail.com>
2019-01-27 19:32:46 -0500

    correctly set org

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,7 +6,19 @@ class AdminController < ApplicationController
   private
 
   def load_organization
-    @organization = Organization.includes(survey: [:questions, :displayable_questions]).find_by(uuid: Organization::FOSTER_DOGS_UUID)
-    @survey = @organization.survey
+    is_admin = false
+
+    [Organization.macc, Organization.foster_dogs].each do |org|
+      if current_user.has_role?(:admin, org)
+        @organization = Organization.includes(survey: [:questions, :displayable_questions]).find_by(uuid: org.uuid)
+        @survey = @organization.survey
+        is_admin = true
+      end
+    end
+
+    unless is_admin
+      flash[:alert] = "You are not permitted to do this. Sorry."
+      redirect_to root_path
+    end
   end
 end
